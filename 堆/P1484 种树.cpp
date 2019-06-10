66ade56eff390a6a27f87d33004f91ba82973fff
ttzztztz <935319340@qq.com>
2019-06-10 21:33:25 +0800

    0610

---
+++ 堆/P1484 种树.cpp
@@ -0,0 +1,71 @@
+﻿#include<iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+
+class Node {
+public:
+	int index, money;
+	Node()
+		: index(0), money(0) {};
+	Node(int i, int m)
+		: index(i), money(m) {};
+	bool operator< (const Node& $2) const {
+		return this->money < $2.money;
+	}
+};
+
+const int MAX = 500005;
+bool visited[MAX];
+int N, K, L[MAX], R[MAX];
+priority_queue<Node> list;
+Node nodeList[MAX];
+
+int main() {
+	cin >> N >> K;
+
+	long long answer = 0;
+	for (int i = 1; i <= N; i++) {
+		int money = 0;
+		cin >> money;
+		Node newNode(i, money);
+		L[i] = i - 1;
+		R[i] = i + 1;
+		list.push(newNode);
+		nodeList[i] = newNode;
+	}
+
+	int offset = 0;
+	while (offset < K && !list.empty()) {
+		Node node;
+		do {
+			node = list.top();
+			list.pop();
+		} while (visited[node.index] && !list.empty());
+
+		if (!visited[node.index]) {
+			if (node.money <= 0) {
+				break;
+			}
+			answer += node.money;
+
+			visited[L[node.index]] = true;
+			visited[R[node.index]] = true;
+
+			int money = nodeList[R[node.index]].money + nodeList[L[node.index]].money - node.money;
+			nodeList[node.index].money = money;
+
+			L[node.index] = L[L[node.index]];
+			R[node.index] = R[R[node.index]];
+
+			R[L[node.index]] = node.index;
+			L[R[node.index]] = node.index;
+
+			list.push({ node.index, money });
+		}
+		offset++;
+	}
+
+	cout << answer;
+	return 0;
+}
---
+++ 堆/P1801 黑匣子.cpp
@@ -0,0 +1,174 @@
+﻿#include<iostream>
+
+enum Relation {
+	L, R
+};
+
+class SplayNode {
+public:
+	int count, value, size;
+	SplayNode* child[2];
+	SplayNode* parent;
+	SplayNode() : count(1), value(0), size(1), parent(nullptr) {
+		child[L] = nullptr;
+		child[R] = nullptr;
+	};
+	SplayNode(SplayNode* p, int v) :value(v), size(1), count(1), parent(p) {
+		child[L] = nullptr;
+		child[R] = nullptr;
+	}
+};
+
+Relation getRelation(SplayNode* node) {
+	if (node->parent) {
+		return node->parent->child[L] == node ? L : R;
+	}
+	else {
+		return L;
+	}
+}
+
+SplayNode* getGrandParent(SplayNode* node) {
+	return node->parent ? node->parent->parent : nullptr;
+}
+
+Relation reverseRelation(Relation r) {
+	return r == L ? R : L;
+}
+
+void setSon(SplayNode* parent, SplayNode* son, Relation relation) {
+	if (parent) {
+		parent->child[relation] = son;
+	}
+	if (son) {
+		son->parent = parent;
+	}
+}
+
+void maintain(SplayNode* node) {
+	if (node) {
+		int leftSize = node->child[L] ? node->child[L]->size : 0;
+		int rightSize = node->child[R] ? node->child[R]->size : 0;
+
+		node->size = leftSize + rightSize + node->count;
+	}
+}
+
+void rotate(SplayNode* node) {
+	if (!node) {
+		return;
+	}
+
+	SplayNode* parent = node->parent;
+	SplayNode* grandParent = getGrandParent(node);
+	SplayNode* child = node->child[reverseRelation(getRelation(node))];
+	Relation nodeRelation = getRelation(node);
+
+
+	setSon(parent, child, nodeRelation);
+	setSon(grandParent, node, getRelation(parent));
+	setSon(node, parent, reverseRelation(nodeRelation));
+
+	maintain(parent);
+	maintain(node);
+}
+
+SplayNode* root;
+
+void splay(SplayNode* curr, SplayNode* target = nullptr) {
+	if (!curr) {
+		return;
+	}
+
+	while (curr && curr->parent != target) {
+		SplayNode* parent = curr->parent;
+		SplayNode* grandparent = getGrandParent(curr);
+
+		if (grandparent) {
+			if (getRelation(parent) == getRelation(grandparent)) {
+				rotate(parent);
+			}
+			else {
+				rotate(curr);
+			}
+		}
+		rotate(curr);
+	}
+
+	if (!target) {
+		root = curr;
+	}
+}
+
+int rank(SplayNode* node) {
+	if (!node) {
+		return 0;
+	}
+	else {
+		return node->child[L] ? node->child[L]->size : 0;
+	}
+}
+
+void insert(SplayNode* curr, int val) {
+	SplayNode* previous = nullptr;
+	while (curr && curr->value != val) {
+		previous = curr;
+		curr = curr->child[val < curr->value ? L : R];
+	}
+
+	if (curr) {
+		curr->count++;
+		maintain(curr);
+	}
+	else {
+		Relation r = val < previous->value ? L : R;
+		previous->child[r] = new SplayNode(previous, val);
+		curr = previous->child[r];
+	}
+	splay(curr);
+}
+
+SplayNode* select(SplayNode* node, int k) {
+	SplayNode* curr = node;
+
+	while (!(k >= rank(curr) && k < rank(curr) + curr->count)) {
+		if (k < rank(curr)) {
+			curr = curr->child[L];
+		}
+		else {
+			k -= rank(curr) + curr->count;
+			curr = curr->child[R];
+		}
+	}
+
+	splay(curr);
+	return curr;
+}
+
+int arr[200005], now = 1, indicator = 0;
+int main() {
+	root = new SplayNode(nullptr, 0);
+	int N = 0, M = 0;
+	std::cin >> M >> N;
+
+	for (int i = 1; i <= M;i++) {
+		std::cin >> arr[i];
+	}
+
+	for (int i = 0; i < N;i++) {
+		int query = 0;
+		std::cin >> query;
+		if (now == 1) {
+			root->value = arr[1];
+			now++;
+		}
+		while (query >= now) {
+			insert(root, arr[now]);
+			now++;
+		}
+		SplayNode* result = select(root, indicator++);
+		std::cout << result->value << std::endl;
+	}
+
+	return 0;
+}
