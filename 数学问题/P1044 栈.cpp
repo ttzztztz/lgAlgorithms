881fab8e293059c364d3e1f15df28e2a834e0159
ttzztztz <935319340@qq.com>
2019-09-07 14:08:51 +0800

    0907

---
+++ 数学问题/P1044 栈.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n = 0;
+    cin >> n;
+    unsigned long long C[2 * n + 1][2 * n + 1];
+
+    for (int i = 1; i <= 2 * n; i++) {
+        C[i][0] = C[i][i] = 1;
+        for (int j = 1; j < i; j++) {
+            C[i][j] = C[i - 1][j] + C[i - 1][j - 1];
+        }
+    }
+
+    cout << C[2 * n][n] - C[2 * n][n - 1];
+
+    return 0;
+}
