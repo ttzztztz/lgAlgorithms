de5c5a644039d175a57564c968f988c50f6c0e30
ttzztztz <935319340@qq.com>
2019-11-10 13:04:52 +0800

    Fix ST Template TLE

--- 模板/P3865 【模板】ST表.cpp
+++ 模板/P3865 【模板】ST表.cpp
@@ -1,42 +1,41 @@
-﻿#include<iostream>
-#include<cmath>
+#include <iostream>
+
 using namespace std;
 
-int N, M;
-const int MAX = 100005, MAXLOGN = 30;
-int dp[MAX][MAXLOGN];
+const int MAXN = 100005, MAXLOGN = 20;
+int dp[MAXN][MAXLOGN + 5], Log2N[MAXN];
 
-inline int max(int a, int b) {
-	return a < b ? b : a;
+void init() {
+    for (int j = 1; j <= MAXLOGN; j++) {
+        for (int i = 1; i + (1 << j) - 1 <= MAXN; i++) {
+            dp[i][j] = max(dp[i][j - 1], dp[i + (1 << (j - 1))][j - 1]);
+        }
+    }
+
+    Log2N[1] = 0;
+    Log2N[2] = 1;
+    for (int i = 3; i < MAXN; i++) {
+        Log2N[i] = Log2N[i / 2] + 1;
+    }
 }
-
-inline void prepare() {
-	int maxJ = log2(N);
-
-	for (int j = 1; j <= maxJ; j++) {
-		for (int i = 1; i + (1 << j) - 1 <= N;i++) {
-			dp[i][j] = max(dp[i][j - 1], dp[i + (1 << (j - 1))][j - 1]);
-		}
-	}
-}
-
 
 int main() {
-	ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
+    int N, M;
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++) {
+        cin >> dp[i][0];
+    }
 
-	cin >> N >> M;
-	for (int i = 1; i <= N;i++) {
-		cin >> dp[i][0];
-	}
+    init();
+    for (int i = 0; i < M; i++) {
+        int start, end;
+        cin >> start >> end;
 
-	prepare();
-	int start = 0, end = 0;
-	for (int i = 0; i < M;i++) {
-		cin >> start >> end;
-		int k = log2(end - start + 1);
+        const int K = Log2N[end - start + 1];
 
-		cout << max(dp[start][k], dp[end - (1 << k) + 1][k]) << endl;
-	}
+        printf("%d\n", max(dp[start][K], dp[end - (1 << K) + 1][K]));
+    }
 
-	return 0;
+    return 0;
 }
